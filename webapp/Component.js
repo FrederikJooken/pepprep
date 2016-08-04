c366b759a6df3133f6f7879b7e3456c50a614ccf
Jooken Frederik <frederik.jooken@b-rail.be>
2016-08-04 13:48:31 +0000

    init

---
+++ webapp/Component.js
@@ -0,0 +1,19 @@
+jQuery.sap.declare("hcm.people.profile.ZHCM_PEP_PROFILEExt.Component");
+
+// use the load function for getting the optimized preload file if present
+sap.ui.component.load({
+	name: "hcm.people.profile",
+	// Use the below URL to run the extended application when SAP-delivered application is deployed on SAPUI5 ABAP Repository
+	url: "/sap/bc/ui5_ui5/sap/HCM_PEP_PROFILE"
+		// we use a URL relative to our own component
+		// extension application is deployed with customer namespace
+});
+
+this.hcm.people.profile.Component.extend("hcm.people.profile.ZHCM_PEP_PROFILEExt.Component", {
+	metadata: {
+		version: "1.0",
+		config: {},
+
+		customizing: {}
+	}
+});
